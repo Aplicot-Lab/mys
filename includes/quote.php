9a2cff7dd25d6e55ddc96aeb499cede9fc824f24
ESC <masuda@e-s-com.jp>
2015-03-09 18:59:47 +0900

    Revert "template commit"
    This reverts commit 431d951509b853ba2f9730f234847315d61cd318.

--- includes/quote.php
+++
@@ -1,6 +0,0 @@
-<?php global $options; ?>
-<div id="quote">
-    <p class="quote"><?php echo $options['call_to_action_one']; ?></p>
-    <p class="quote-second"><?php echo $options['call_to_action_two']; ?></p> 
-    <a href="<?php echo $options['call_to_action_link']; ?>" class="action-button"><span><?php echo $options['call_to_action_button']; ?></span></a>
-</div> <!--  #quote  -->
